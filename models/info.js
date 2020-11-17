903ec9979d96fd7c3f6611f2600dc809519387c7
Lucho Web <webfacilco@gmail.com>
2020-11-17 11:54:45 -0500

    eslint added

---
+++ models/info.js
@@ -0,0 +1,8 @@
+var Info = function(name, addressLineOne, addressLineTwo, shortDescriptionTop, aboutFooter, socialLinks) {
+  this.name = name;
+  this.addressLineOne = addressLineOne;
+  this.addressLineTwo = addressLineTwo;
+  this.shortDescriptionTop = shortDescriptionTop;
+  this.aboutFooter = aboutFooter;
+  this.socialLinks = socialLinks;
+}
